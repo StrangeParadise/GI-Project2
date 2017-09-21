67bfbdf03511ee04c5fa9ac4f9c8f78c4d5d62fd
Yujing Mark Jiang <yujingj@student.unimelb.edu.au>
2017-09-21 15:01:40 +1000

    T

---
+++ Project 2/Assets/Scripts/GameMapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+
+public class GameMapGenerator : MonoBehaviour {
+
+    public string mapFile;
+
+    void LoadFile() {
+
+        return;
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
